ceeef2c453b9ae0d7f2a6c91a61c26d9153e6579
Alan Antonio Ramos Herrera <alanrahe06@gmail.com>
2019-01-09 14:36:29 -0600

    POST method

--- src/main/java/com/cwgx/newhorizon/rest/UserResource.java
+++ src/main/java/com/cwgx/newhorizon/rest/UserResource.java
@@ -3,14 +3,14 @@ package com.cwgx.newhorizon.rest;
 import com.cwgx.newhorizon.model.User;
 import com.cwgx.newhorizon.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
+@RestController
 public class UserResource {
     @Autowired
     private UserRepository userRepository;
@@ -21,4 +21,13 @@ public class UserResource {
         return userRepository.findAll();
     }
 
+    /*@RequestMapping(path = "/users/post", method = RequestMethod.POST)
+    public ResponseEntity<User> update(@RequestBody User user){
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }*/
+
+    @PostMapping("/users/post")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
 }
