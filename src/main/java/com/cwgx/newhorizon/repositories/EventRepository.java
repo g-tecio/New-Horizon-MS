48691cf7528f302fb64afdd7b134f6d26f71fe0b
Marco A. Mercado Lugo <marco.mercado@g-tec.io>
2019-01-10 10:22:14 -0600

    Event post

---
+++ src/main/java/com/cwgx/newhorizon/repositories/EventRepository.java
@@ -0,0 +1,8 @@
+package com.cwgx.newhorizon.repositories;
+
+import com.cwgx.newhorizon.model.Event;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventRepository extends CrudRepository<Event, Integer> {
+
+}
---
+++ src/main/java/com/cwgx/newhorizon/rest/EventResource.java
@@ -0,0 +1,29 @@
+package com.cwgx.newhorizon.rest;
+
+
+import com.cwgx.newhorizon.model.Event;
+import com.cwgx.newhorizon.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RestController
+public class EventResource {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @RequestMapping(path = "/events", method = RequestMethod.GET)
+    public @ResponseBody
+    Iterable<Event> getAllEvents() {
+        // This returns a JSON or XML with the users
+        return eventRepository.findAll();
+    }
+
+    @PostMapping("/events/post")
+    Event newUser(@RequestBody Event newEvent){
+        return eventRepository.save(newEvent);
+    }
+
+}
