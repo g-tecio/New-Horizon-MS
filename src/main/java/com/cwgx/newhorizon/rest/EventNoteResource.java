a1d115ccab7662b727adf70e68040ce6128204d3
Alberto Camarena <alberto.camarena@g-tec.io>
2019-01-11 14:42:51 -0600

    Event Note

--- src/main/java/com/cwgx/newhorizon/rest/EventNoteResource.java
+++ src/main/java/com/cwgx/newhorizon/rest/EventNoteResource.java
@@ -7,7 +7,6 @@ import com.cwgx.newhorizon.repositories.SocialLinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 @Controller
 @RestController
 public class EventNoteResource {
